63e0b9e74c99de73a39c7276bdf3454e24ced7db
Vijay <63943580+Vijay1126@users.noreply.github.com>
2020-05-07 22:00:09 -0400

    Tut-3-CodeNinja

--- newfire/functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin'
-
-admin.initializeApp(functions.config().firebase)
-
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send('Hello from Firebase!\n\n');
-});
