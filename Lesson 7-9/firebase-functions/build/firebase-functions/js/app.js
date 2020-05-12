485786c9b4564013918e3862029997642720a64e
Vijay <63943580+Vijay1126@users.noreply.github.com>
2020-05-11 20:16:18 -0400

    Lesson 7-9

---
+++ Lesson 7-9/firebase-functions/build/firebase-functions/js/app.js
@@ -0,0 +1,16 @@
+
+const requestModal = document.querySelector('.new-request');
+const requestLink = document.querySelector('.add-request');
+
+// open request modal
+requestLink.addEventListener('click', () => {
+  requestModal.classList.add('open');
+});
+
+// close request modal yoooo
+requestModal.addEventListener('click', (e) => {
+  if (e.target.classList.contains('new-request')) {
+    requestModal.classList.remove('open');
+  }
+});
+
